02a8f036a7eb2656f060b522777fbaf7c4228edb
ovvn <omazinov@gmail.com>
2019-07-28 20:36:52 +0300

    Add deployment scripts.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t ovvn/multi-client:latest -t ovvn/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ovvn/multi-server:latest -t ovvn/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ovvn/multi-worker:latest -t ovvn/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ovvn/multi-client:latest
+docker push ovvn/multi-server:latest
+docker push ovvn/multi-worker:latest
+
+docker push ovvn/multi-client:$SHA
+docker push ovvn/multi-server:$SHA
+docker push ovvn/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=ovvn/multi-server:$SHA
+kubectl set image deployments/client-deployment client=ovvn/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ovvn/multi-worker:$SHA
